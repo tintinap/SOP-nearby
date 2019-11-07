7fc5bde6f15f74f2fd67a3258247d70410c68119
tintinap <31465850+tintinap@users.noreply.github.com>
2019-11-07 10:12:24 +0700

    updated port to port 8989 for django app

--- SOP/manage.py
+++ SOP/manage.py
@@ -8,7 +8,7 @@ import py_eureka_client.eureka_client as eureka_client
 def main():
     eureka_client.init(eureka_server="http://localhost:8761/eureka/",
                         app_name="SUGGEST-NEARBY-API",
-                        instance_port=8888
+                        instance_port=8989
     )
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SOP.settings')
     try:
