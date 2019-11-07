0a08bcb299ad9ea9cb17e62430bc6739bd019dd1
tintinap <31465850+tintinap@users.noreply.github.com>
2019-11-07 10:03:58 +0700

    added services
    added Discovery Servicer
    added Service gateway
    added Eureka to Django app

--- SOP/manage.py
+++ SOP/manage.py
@@ -2,9 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import py_eureka_client.eureka_client as eureka_client
 
 
 def main():
+    eureka_client.init(eureka_server="http://localhost:8761/eureka/",
+                        app_name="SUGGEST-NEARBY-API",
+                        instance_port=8888
+    )
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SOP.settings')
     try:
         from django.core.management import execute_from_command_line
