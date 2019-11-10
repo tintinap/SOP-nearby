fe181211aa2ff4f68420be426f359da7460b807e
xzsawq47 <60070074@kmitl.ac.th>
2019-11-10 19:34:25 +0700

    almost done

--- SOP/nearby/input.py
+++ SOP/nearby/input.py
@@ -12,8 +12,7 @@ class Places:
         self.ip_id = ip_id  # IP is a username of user
         self.place_api = []  # array for sorted place
 
-    def r_user_to_place(self, user_lat, user_lon, place_lat,
-                        place_lon):  # need edit to match with places api and user location that get
+    def r_user_to_place(self, user_lat, user_lon, place_lat,place_lon):  # need edit to match with places api and user location that get
         dlat = math.radians(place_lat - user_lat)  # should be self.user.lat self.place.lat
         dlon = math.radians(place_lon - user_lon)
         a = math.pow(math.sin(dlat / 2), 2) + math.cos(math.radians(place_lat)) * math.cos(
@@ -23,29 +22,24 @@ class Places:
         d = round(d, 3)
         return d
 
-    # def find_nearest_place(self):  # need edit to match with places api that get
-    #     nearest_place = None
-    #     r = 0
-    #     for i in self.places:
-    #         if self.r_user_to_place(self.user_location.lat, self.user_location.lon, self.places[i].lat,
-    #                                 self.places[i].lon) < self.r_user_to_place(self.places[i].viewport.lat1,
-    #                                                                            self.places[i].viewport.lon1,
-    #                                                                            self.places[i].viewport.lat2,
-    #                                                                            self.places[i].viewport.lon2) / 2
-    #             if r >= self.r_user_to_place(self.user_location.lat, self.user_location.lon, self.places[i].lat,
-    #                                          self.places[i].lon) and r != 0:
-    #                 nearest_place = self.places[i]
-    #     return nearest_place  # save r to this var too
+    def find_nearest_place(self):  # need edit to match with places api that get
+        nearest_place = None
+        r = 0
+        for i in self.places:
+            if self.r_user_to_place(self.user_lat, self.user_lon, self.places[i]['geometry']['location']['lat'].lat,self.places[i]['geometry']['location']['lon']) < self.r_user_to_place(self.places[i]['viewport']['northest']['lat'],self.places[i]['viewport']['northest']['lon'],self.places[i]['viewport']['southwest']['lat'],self.places[i]['viewport']['southwest']['lon']) / 2:
+                if r >= self.r_user_to_place(self.user_lat, self.user_lon, self.places[i]['geometry']['location']['lat'],self.places[i]['geometry']['location']['lon']) and r != 0:
+                    nearest_place = self.places[i]
+        return nearest_place  # save r to this var too
 
-    # def rank_place(self):  # can upgrad performance by check with tag #need edit to match with places api that get
-    #     place_saved = []
-    #     place_unsaved = []
-    #     for i in self.places:
-    #         if self.places[i] in db.getall(place):
-    #             place_saved.append(self.places[i])
-    #         else:
-    #             place_unsaved.append(self.places[i])
-    #     self.place_api = place_saved.sort + place_unsaved.sort  # place_saved sort by rank in db #place_unsaved sort by distance
+    def rank_place(self):  # can upgrad performance by check with tag #need edit to match with places api that get
+        place_saved = []
+        place_unsaved = []
+        for i in self.places:
+            if self.places[i] in db.getall(place): #place nameก้ได้
+                place_saved.append(self.places[i])
+            else:
+                place_unsaved.append(self.places[i])
+        self.place_api = place_saved.sort + place_unsaved.sort  # place_saved sort by rank in db #place_unsaved sort by distance
 
 
 # test
--- SOP/nearby/views.py
+++ SOP/nearby/views.py
@@ -3,8 +3,27 @@ from django.http import HttpResponse
 from nearby.models import Place, User
 from nearby.input import Places
 from . import place_nearby
+import geocoder
+import time
 
 import socket
+def compute_rank_point(visited_count,count_time_avg):
+    rank = visited_count*0.8+count_time_avg*0.2
+    return rank
+def get_user_location():
+
+    my_location = geocoder.ip('me')
+    print(my_location.latlng)
+    get_user_location.user_lat = str(my_location.latlng[0])
+    get_user_location.user_lon = str(my_location.latlng[1])
+
+    # print(user_lat)
+    # print(user_lon)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('google.com', 80))
+    print("Your Computer IP Address is: " + s.getsockname()[0])
+    get_user_location.lo = s.getsockname()[0]
 
 def index(request):
     count_time = 0
@@ -13,94 +32,83 @@ def index(request):
     place_type = "hospital" #example
     api_key = "AIzaSyBbq0VljhDuyG5TkqguBiL9Wnnq-_BTa1k"
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(('google.com', 80))
-    print("Your Computer IP Address is: " + s.getsockname()[0])
-    print(request.GET.get('lat'))
-    print(request.GET.get('long'))
-    user_lat = request.GET.get('lat')
-    user_lon = request.GET.get('long')
-    # context = {
-    #     'ip': s.getsockname()[0]
-    # }
+    
+    get_user_location()
 
-    place_api = place_nearby.places_nearby(user_lat,user_lon,place_type,api_key)
+    # my_location = geocoder.ip('me')
+    # print(my_location.latlng)
+    # user_lat = str(my_location.latlng[0])
+    # user_lon = str(my_location.latlng[1])
+
+    # print(user_lat)
+    # print(user_lon)
+
+    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # s.connect(('google.com', 80))
+    # print("Your Computer IP Address is: " + s.getsockname()[0])
+    # print(request.GET.get('lat'))
+    # print(request.GET.get('long'))
+
+    place_api = place_nearby.places_nearby(get_user_location.user_lat,get_user_location.user_lon,place_type,api_key)
     print(place_api)
 
 
-    # user = Places(user_lat,user_lon, place_api, 112)
+    user = Places(get_user_location.user_lat,get_user_location.user_lon, place_api, get_user_location.lo)
 
-
-
-
-
-
-
-    # print(11111111111111)
-    place = Place.objects.filter(idplace=1).values()
-    print(222222222222)
-    print(place[0]['place_name'])
-    user = Places(1, 2, 112)  # get user_locate,place_api,ip_id
     try: 
         db = User.objects.get(ip=user.ip_id) # get IP from User db
-        print(3333333333333)
     except:
-        print(44444444444)
         User.objects.create(ip=user.ip_id) # save to db
 
+    while service_running():
+        # my_location = geocoder.ip('me')
+        # print(my_location.latlng)
+        # user_lat = str(my_location.latlng[0])
+        # user_lon = str(my_location.latlng[1])
+        # user.user_lat = user_lat
+        # user.user_lon = user_lon
+        get_user_location()
+        user.user_lat = get_user_location.user_lat
+        user.user_lon = get_user_location.user_lon
 
-        # if 'op' not in place:
-            # return HttpResponse("STP")
+        if user.find_nearest_place != None:  # in some place #place_view_port() function to find nearest place
+            user.places = place_nearby.places_nearby(user.user_lat,user.user_lon,place_type,api_key)  # get place location
+            user.rank_place
+            # send_place_to_output(place_api)???
+            current_place = user.find_nearest_place['name']
 
-    return HttpResponse(place)
+            while user.find_nearest_place['name'] == current_place:
+                get_user_location()
+                user.user_lat = get_user_location.user_lat
+                user.user_lon = get_user_location.user_lon
+                count_time += 1
+                time.sleep(1)  # not sure function
+            user.places = place_nearby.places_nearby(user.user_lat,user.user_lon,place_type,api_key) #get new api after get out place
+            user.rank_place
+            # send_place_to_output(place_api)
 
-# def main():
-#     count_time = 0
-#     count_time_to_get_api = 0
+        else: #outside the place
+            if count_time_to_get_api == 60:
+                get_user_location()
+                user.user_lat = get_user_location.user_lat
+                user.user_lon = get_user_location.user_lon
+                user.places = place_nearby.places_nearby(user.user_lat,user.user_lon,place_type,api_key) #get new api after pass by 1 min
+                user.rank_place
+                # send_place_to_output(place_api)
+                count_time_to_get_api = 0
 
-#     db.save(user.ip_id)  # save from db
+            else:
+                count_time_to_get_api += 1
+            time.sleep(1)
+        
+        if count_time >= 900: # more than 15 mins #get data from db current place
+            count_time_avg = (db.get(current_place).count_time*db.get(current_place).visited_count+count_time)/(db.get(current_place).visited_count+1)
+            visited_count = db.get(current_place).visited_count+1 
+            rank_point = compute_rank_point(visited_count,count_time_avg)#compute rank point
+            db.save(current_place,visited_count,count_time,rank_point)#save to db
+        
+        count_time = 0
+        visited_count = 0
+        current_place = ""
 
-#     while service_running():
-#         user.user_location = get_user_location()  # get user ip???
-
-#         if user.find_nearest_place != None:  # in some place #place_view_port() function to find nearest place
-#             user.places = from_google_api(location=current_user_location, radius=5000)  # get place location
-#             user.rank_place
-#             # send_place_to_output(place_api)???
-#             current_place = user.find_nearest_place
-
-#             while user.find_nearest_place != None:
-#                 user.user_location = get_user_location()
-#                 count_time += 1
-#                 time.sleep(1)  # not sure function
-
-#         else:  # outside the place
-#             if count_time_to_get_api == 60:
-#                 user.places = from_google_api(location=current_user_location,
-#                                               radius=5000)  # get new api after pass by 1 min
-#                 user.rank_place
-#                 # send_place_to_output(place_api)
-#                 count_time_to_get_api = 0
-
-#             else:
-#                 count_time_to_get_api += 1
-#             time.sleep(1)
-
-#         places = from_google_api(location=current_user_location, radius=5000)  # get new api after get out place
-#         user.rank_place
-#         # send_place_to_output(place_api)
-
-#         if count_time >= 900:  # more than 15 mins #get data from db current place
-#             count_time_avg = (db.get(current_place).count_time * db.get(current_place).visited_count + count_time) / (
-#                         db.get(current_place).visited_count + 1)
-#             visited_count = db.get(current_place).visited_count + 1
-#             rank_point = compute_rank_point(visited_count, count_time_avg)  # compute rank point
-#             db.save(current_place, visited_count, count_time, rank_point)  # save to db
-
-#         count_time = 0
-#         visited_count = 0
-#         current_place = ""
-#
-#
-# def compute_rank_point(visited_count, count_time_avg):  # thinking...
-#
+    return HttpResponse(place_api)
