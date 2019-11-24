1d486fab4944caf12ed1508b23a51e582bbab8f1
tintinap <31465850+tintinap@users.noreply.github.com>
2019-11-25 00:11:22 +0700

    fixed

--- SOP/nearby/urls.py
+++ SOP/nearby/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('nearby/<str:type>', views.index, name='index'),
     # path('index/', views.index, name='index'),
-    path('location/', views.get_user_location, name='location'),
+    path('location/', views.index, name='index'),
+    # path('location/', views.get_user_location, name='location'),
     path('search/', views.search, name='search')
 ]
